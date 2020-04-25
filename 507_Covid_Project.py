1ecd526b618413d1660d293bd1d3e3129e46c4b1
Katherine Garrison <garrisok@umich.edu>
2020-04-25 18:13:00 -0400

    Adding all files

---
+++ 507_Covid_Project.py
@@ -0,0 +1,158 @@
+#################################
+##### Name: Katherine Garrison ##
+##### Uniqname: garrisok  #######
+#################################
+
+from bs4 import BeautifulSoup
+import requests
+import json
+import urllib.request
+import shutil
+import datetime
+import plotly.graph_objs as go
+
+# import dailyCovid19Data as covidOTD
+
+
+def downloadEuroDataFile():
+    ''' Sends request to url and saves data to json file in local dir.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    none
+    '''
+    url = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json'
+    fileName = 'dailyCovid19Data.json'
+    with urllib.request.urlopen(url) as response, open(fileName, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+
+def convertEuroDataToDict():
+    ''' Converts json data to dict format.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    none
+    '''
+    with open('dailyCovid19Data.json', 'r') as file:
+        return json.load(file)
+
+
+def findRecordsForCountry(countryCode: str, records: list):
+    '''Matches the countryCode ('geoID' key) with the countryCode of items in records param.
+
+    Parameters
+    ----------
+    countryCode: string
+    records: list of dicts
+
+    Returns
+    -------
+    list
+        a list of tuples that match the countryCode param
+    '''
+    #return [x for x in records if x['geoId'] == countryCode]
+    output = []
+    for x in records:
+        if x['geoId'] == countryCode:
+            output.append(x)
+    return output
+
+
+def findRecordWithMaxCases(records: list):
+    getCasesForRecord = lambda record: int(record['cases'])
+    #print(getCasesForRecord(records[0]))
+    return max(records, key=getCasesForRecord)
+
+
+def findRecordWithMaxDeaths(records: list):
+    getDeathsForRecord = lambda record: int(record['deaths'])
+    #print(getCasesForRecord(records[0]))
+    return max(records, key=getDeathsForRecord)
+
+
+def findMostRecentRecords(numDays: int, records: list):
+    convertRecordToDate = lambda record: datetime.date(int(record['year']), int(record['month']), int(record['day']))
+    sortedRecords = sorted(records, key=convertRecordToDate)
+    return sortedRecords[-numDays:]
+
+
+def addDateObjectToRecords(records: list):
+    for record in records:
+        record['date'] = datetime.date(int(record['year']), int(record['month']), int(record['day']))
+    return records
+
+
+def createCasesPlot(records):
+
+    countryName = records[0]['countriesAndTerritories'].replace('_', ' ')
+    xAxis = [record['date'].strftime("%B %d") for record in records]
+        #print('xAxis=', xAxis)
+        # xAxis = []
+        # for record in recentRecordsWithDate:
+        #     xAxis.append(record['date'].strftime("%B %d"))
+
+        #xAxis = map(lambda record: record['date'].strftime("%B %d"), recentRecordsWithDate)
+
+    yAxis = [int(record['cases']) for record in records]
+    fig = go.Figure(data=go.Scatter(x=xAxis, y=yAxis, mode='lines+markers', marker={'symbol':'triangle-down', 'size':17, 'color':'navy'}, line={'color': 'lightskyblue', 'width':3, 'dash':'solid'}))
+    fig.update_layout(title=f'Recent Cases in {countryName}', xaxis_title='Date', yaxis_title='Cases')
+    fig.write_html("scatter-cases.html", auto_open=True)
+
+
+def createDeathsPlot(records):
+
+    countryName = records[0]['countriesAndTerritories'].replace('_', ' ')
+    xAxis = [record['date'].strftime("%B %d") for record in records]
+    yAxis = [int(record['deaths']) for record in records]
+    fig = go.Figure(data=go.Scatter(x=xAxis, y=yAxis, mode='lines+markers', marker={'symbol':'circle', 'size':21, 'color':'darkred'}, line={'color': 'black', 'width':3, 'dash':'solid'}))
+    fig.update_layout(title=f'Recent Deaths in {countryName}', xaxis_title='Date', yaxis_title='Deaths')
+    fig.write_html("scatter-deaths.html", auto_open=True)
+
+
+def startInteractivePrompt():
+    covidOTD = convertEuroDataToDict()
+    records = covidOTD['records']
+    while True:
+        response = input("Enter Country Code (2 Letters) or 'exit': ")
+        countryRecords = findRecordsForCountry(response, records)
+        if response == 'exit':
+            break
+        if not countryRecords:
+            print("Hey that's not a valid country code >:(")
+            continue
+        daysResponse = input("Enter Number of Days to retroactively report : ")
+        while not daysResponse.isdigit():
+            print('Hey that\'s not a positive integer >:(')
+            daysResponse = input("Enter Number of Days: ")
+        recentRecords = findMostRecentRecords(int(daysResponse), countryRecords)
+        recentRecordsWithDate = addDateObjectToRecords(recentRecords)
+        #print(recentCountryRecords)
+        print("-------------")
+        print("Requested days reported for " + recentRecords[0]['countriesAndTerritories'].replace("_", " "))
+        print("-------------")
+        print(f'{"Date":30}{"New Cases":15}{"New Deaths":15}')
+        for record in recentRecordsWithDate:
+            print(f"{record['date'].strftime('%A, %B %d, %Y'):30}{int(record['cases']):<15,d}{int(record['deaths']):<15,d}")
+        # Show user plotly visualizations
+        createCasesPlot(recentRecordsWithDate)
+        createDeathsPlot(recentRecordsWithDate)
+
+    #print(findRecordWithMaxDeaths(findRecordsForCountry('MX', records)))
+
+
+if __name__ == '__main__':
+    downloadEuroDataFile()
+    startInteractivePrompt()
+
+    # print(findDataForCountry('BR', records))
+
+
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# 507_finalproject
-COVID-19 Project
+# About COVID-19 Project
+
+This is a simple program, aggregating specified data from COVID-19 confirmed cases and deaths by country. Country and dates are chosen by user through input.
+
+## How to work it
+
+User is prompted to specify the Alpha2 country code to attain data on.
+    See "Country_Alpha2.txt” file for list of country names and alpha-2 codes.
+    For example, to request US data, user input is "US".
+
+### Data Source
+
+When the "507_Covid_Project.py" is ran, before user input interaction the data will be downloaded as a JSON file in the user's current directory as a file named "dailyCovid19Data.json"
+
+**European Centre for Diease Prevention and Control (ECDC)** - *Main Site* - [ECDC](https://www.ecdc.europa.eu/en)
+
+**ECDC Daily Data on COVID-19 Cases ** - *Data Site* - [ECDC](https://opendata.ecdc.europa.eu/covid19/casedistribution/json)
+
+### Prerequisites
+
+Python packages needed to run this program.
+
+```
+from bs4 import BeautifulSoup
+import requests
+import json
+import urllib.request
+import shutil
+import datetime
+import plotly.graph_objs as go
+```
+
+### User Input
+
+User is then prompted to specify the amount of days to retroactively report on. Requests include present day.
+Below example reuqests information for US and the last 5-days.
+
+Input:
+```
+Enter Country Code (2 Letters) or 'exit': US
+```
+
+Next input request:
+
+```
+Enter Number of Days to retroactively report : 5
+```
+Upon selection of number of days, output will look similar to displayed table below. User will be able to continue requesting data with the input loop, or exit the program.
+Two windows will open in an internet browser showing a graph for "Recent Deaths in United States of America" and "Recent Cases in United States of America":
+
+```
+-------------
+Requested days reported for United States of America
+-------------
+Date                          New Cases      New Deaths
+Tuesday, April 21, 2020       28,065         1,857
+Wednesday, April 22, 2020     37,289         2,524
+Thursday, April 23, 2020      17,588         1,721
+Friday, April 24, 2020        26,543         3,179
+Saturday, April 25, 2020      21,352         1,054
+Enter Country Code (2 Letters) or 'exit':
+```
+![Confirmed Cases](link-to-image)
+![Confirmed Deaths](link-to-image)
+
+## Authors
+
+* **Katherine Garrison** - *Current work* - [GitHub](https://github.com/garrisok)
+
